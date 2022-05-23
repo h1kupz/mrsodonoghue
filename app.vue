82fb86d92c2be97f810d6f8e945e73454ad6668b
h1kupz <h1kupz3z@gmail.com>
2022-05-23 21:07:09 +0700

    Netlify

--- app.vue
+++ app.vue
@@ -12,13 +12,4 @@
 
 <script lang="ts" setup>
 import "./assets/css/tailwind.css";
-export default {
-  head() {
-    return {
-      script: [
-        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-      ],
-    };
-  },
-};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,3 +7,15 @@
     />
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+      ],
+    };
+  },
+};
+</script>
