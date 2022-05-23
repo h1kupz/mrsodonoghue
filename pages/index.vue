8382b5b2b69f7202311d31c3722116ade5143732
h1kupz <h1kupz3z@gmail.com>
2022-05-23 20:51:13 +0700

    Minor Index Change

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="modgrow">
     <img
       class="w-4/5 self-center mb-36"
-      src="../assets/images/ModBadge.png"
+      src="@/assets/images/ModBadge.png"
       alt="alt"
     />
   </div>
