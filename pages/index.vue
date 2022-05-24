b80381a03db347c318933071bab24fc54ee8a8ad
h1kupz <h1kupz3z@gmail.com>
2022-05-24 19:25:47 +0700

    Width & Logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modgrow">
     <img
-      class="w-4/5 self-center mb-36"
+      class="lg:w-1/2 md:w-4/6 w-4/5 self-center mb-36"
       src="@/assets/images/ModBadge.png"
       alt="alt"
     />
