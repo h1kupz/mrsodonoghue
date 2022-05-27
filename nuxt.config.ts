08cd989bcd57f8acb2085a821c083802b041a1a1
h1kupz <h1kupz3z@gmail.com>
2022-05-27 16:00:35 +0700

    Favicon Tutle Netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,35 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ["~/assets/css/tailwind.css"],
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                },
-            },
-        },
+  head: {
+    title: "Mrs O'Donoghue",
+    script: [
+      {
+        hid: "netlify",
+        src: "https://identity.netlify.com/v1/netlify-identity-widget.js",
+      },
+    ],
+    htmlAttrs: {
+      lang: "en",
     },
-    modules: []
-})
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+    ],
+  },
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  modules: [],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,14 @@
 export default {
   head() {
     return {
+      title: "Mrs O'Donoghue",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: `favicon.png`,
+        },
+      ],
       script: [
         { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
       ],
